012d4797a7aa69daeb78a5de994c88d6e31fe61c
Diego Quintanilla <diego.quintanilla@proton.me>
2024-02-07 22:24:06 -0500

    start grid component story work

---
+++ src/stories/grid.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { GeneralGridComponent } from '../app/shared/grids/general-grid/general-grid.component';
+
+const meta: Meta<GeneralGridComponent> = {
+  component: GeneralGridComponent,
+};
+
+export default meta;
+type Story = StoryObj<GeneralGridComponent>;
+
+export const BaseGeneralGridStory: Story = {
+  args: {},
+};
