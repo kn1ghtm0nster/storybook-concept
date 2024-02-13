f6c4ff5fb26f3139484951da6e1a9299115d51de
Diego Quintanilla <diego.quintanilla@proton.me>
2024-02-12 22:26:03 -0500

    Refactor general-dialog component to dynamically load components

--- src/app/shared/dialogs/general-dialog/general-dialog.component.ts
+++ src/app/shared/dialogs/general-dialog/general-dialog.component.ts
@@ -7,21 +7,30 @@ import {
   ViewChild,
   ComponentFactoryResolver,
   Type,
+  ViewContainerRef,
+  AfterViewInit,
 } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 import { DialogInput } from '../../../interfaces/dialog.interface';
+import { GridConfig } from 'src/app/interfaces/grid.interface';
+import { defaultGridConfig } from '../../variables/main-grid.variables';
 
 @Component({
   selector: 'app-general-dialog',
   templateUrl: './general-dialog.component.html',
   styleUrls: ['./general-dialog.component.scss'],
 })
-export class GeneralDialogComponent implements OnInit {
+export class GeneralDialogComponent implements OnInit, AfterViewInit {
   @Input() dialogInput: DialogInput | null = null;
 
   @Output() save = new EventEmitter<void>();
   @Output() cancel = new EventEmitter<void>();
+
+  @ViewChild('dialogContent', { read: ViewContainerRef })
+  dialogContentContainer!: ViewContainerRef;
+
+  gridConfigurationObject: GridConfig<any> | null = null;
 
   // contains the component to be rendered dynamically
   dynamicComponent: Type<any> | null = null;
@@ -31,16 +40,36 @@ export class GeneralDialogComponent implements OnInit {
     private resolver: ComponentFactoryResolver
   ) {}
 
-  ngOnInit(): void {
-    this.dynamicComponent = this.dialogInput?.component || null;
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.dialogInput && this.dialogInput?.component) {
+        this.loadDynamicComponent(this.dialogInput?.component);
+      }
+    });
   }
 
-  onSave(): void {
+  public loadDynamicComponent(component: Type<any>): void {
+    const componentFactory = this.resolver.resolveComponentFactory(component);
+
+    this.dialogContentContainer.clear();
+    const componentRef =
+      this.dialogContentContainer.createComponent(componentFactory);
+
+    if (this.gridConfigurationObject) {
+      componentRef.instance.gridConfig = this.gridConfigurationObject;
+    } else {
+      componentRef.instance.gridConfig = defaultGridConfig;
+    }
+  }
+
+  public onSave(): void {
     this.save.emit();
     this.dialogRef.close();
   }
 
-  onCancel(): void {
+  public onCancel(): void {
     this.cancel.emit();
     this.dialogRef.close();
   }
