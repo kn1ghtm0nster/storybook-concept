01530129beda9873b30fe30e8482edbce4a33743
Diego Quintanilla <diego.quintanilla@proton.me>
2024-02-13 13:26:24 -0500

    Add EtlDialogComponent and ModelDialogComponent

---
+++ src/app/shared/dialogs/etl-dialog/etl-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-etl-dialog',
+  templateUrl: './etl-dialog.component.html',
+  styleUrls: ['./etl-dialog.component.scss']
+})
+export class EtlDialogComponent {
+
+}
---
+++ src/app/shared/dialogs/model-dialog/model-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-dialog',
+  templateUrl: './model-dialog.component.html',
+  styleUrls: ['./model-dialog.component.scss']
+})
+export class ModelDialogComponent {
+
+}
